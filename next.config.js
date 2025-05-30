b1706f05b2949d505a32e478aa438a1a577fa536
sadpig <177748749@qq.com>
2025-05-30 15:15:39 +0800

    静态资源basepath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { transpilePackages: ['antd-mobile'], basePath: '/m', };
+const nextConfig = { transpilePackages: ['antd-mobile'], basePath: '/m', assetPrefix: '/m' };
 
 module.exports = nextConfig;
